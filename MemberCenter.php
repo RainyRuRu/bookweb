00c31f12d8ff3a177c446eb7d6475a807e6e5426
System Administrator <root@TsaitekiMacBook-Air.local>
2015-11-04 14:10:18 +0800

    add Member Center index page!

---
+++ MemberCenter.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<link rel=stylesheet type="text/css" href="MemberCenter.css">
+</head>
+<body>
+	<div style="height:50px;background-color:lightgray">暫時的header</div>
+	<div class="container">
+		<div class="MemberTitle">會員中心</div>
+		<div class="MemberBtns">
+			<button type="button" class="btn btn-info InfoBtn">個人資料修改</button>
+			<button type="button" class="btn btn-success HistoryBtn">個人交易紀錄</button>
+		</div>
+	</div>
+
+</body>
+
+</html>
