8a479e2009dad4c21101caa7441fe4e5d1533c74
francisyang0119 <francis.yang0119@gmail.com>
2015-11-04 21:37:59 +0800

    add homepage php & css

---
+++ HomePage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<link rel=stylesheet type="text/css" href="CSS/HomePage.css">
+</head>
+<body>
+	<div style="height:50px;background-color:lightgray">暫時的header
+		<button type="button" class="btn btn-login LoginBtn">登入</button>
+	</div>
+	<div class="container">
+		<div class="HomePageTitle">高師大校內二手書交易網</div>
+		<div class="SearchBox">
+			<input type="text" class="form-control SearchInput">
+			<button type="button" class="btn btn-search SearchBtn">搜尋</button>
+		</div>
+
+	</div>
+
+</body>
+
+</html>
