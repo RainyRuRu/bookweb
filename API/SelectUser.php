ee2d5dcb052b4be58b22eedd3cc8b7c0f37e54a1
Rainy <believeu123@gmail.com>
2015-11-09 17:32:40 +0800

    add API SelectUser.php

---
+++ API/SelectUser.php
@@ -0,0 +1,50 @@
+<?php
+
+//透過u_id搜尋userInfo中的資料
+
+$u_id = $_POST['u_id'];
+
+/*
+test data
+$u_id = '1';
+*/
+
+$result = false;
+$error = null;
+$data = null;
+
+try {
+
+	$dsn = 'mysql:dbname=bookweb;host=140.127.74.164';
+	$user = 'bookweb';
+	$password = '1234';
+	
+	$db = new PDO($dsn, $user, $password);
+	$db->exec("set names utf8");
+    
+    $stmt = $db->prepare('SELECT * from `userinfo` WHERE u_id=:u_id');
+    $stmt->bindParam(":u_id", $u_id);
+
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row != false) {
+        $data = $row;
+        $result = true;
+    } else {
+        $error = "data can't find";
+    }
+
+} catch (PDOException $e) {
+	 $error = $e;
+}
+
+$result = array(
+	"result" => $result,
+    "data" => $data,
+	"responseDetail" => $error,
+);
+
+$json_result = json_encode($result);
+
+echo $json_result;
