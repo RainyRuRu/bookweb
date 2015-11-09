28b52a29af89c240b5e41b97bbc546c9d6565e62
Rainy <believeu123@gmail.com>
2015-11-09 16:59:18 +0800

    add comment in MemberCenter.js

--- JS/MemberCenter.js
+++ JS/MemberCenter.js
@@ -1,5 +1,8 @@
+/*
+	AJAX 戳API - UpdateUser.php
+*/
 function UpdateUserInfo() {
-	var u_id = '1';
+	var u_id = '1'; //id等加上session後修改
 	var name = document.getElementsByName('name')[0].value;
 	var dept = document.getElementsByName('dept')[0].value;
 	var degree = document.getElementsByName('degree')[0].value;
@@ -7,11 +10,15 @@ function UpdateUserInfo() {
 	var phone = document.getElementsByName('phone')[0].value;
 	var address = document.getElementsByName('address')[0].value;
 
+	//JS的AJAX開始
+
 	var xhttp = new XMLHttpRequest();
 
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			//接收API的回傳值 -- 是JSON
 			if (xhttp.responseText.result = "true"){
+				//若回傳的值為成功，開啟成功訊息
 				var alert = document.getElementById('alert');	
 				alert.style.display = 'inline';
 			}
@@ -30,10 +37,19 @@ function UpdateUserInfo() {
 	xhttp.send(data);
 }
 
+/*
+	確認輸入參數非空白
+	如果是空白則顯示錯誤訊息
+
+	全部都非空白呼叫UpdateUserInfo方法
+*/
 function checkField(){
+	
 	var alert = document.getElementById('alert');
 	alert.style.display = 'none';
+
 	var checkResult = true;
+	
 	var data = document.getElementsByTagName('input');
 	for (var i = 0 ; i < data.length ; i++) {
 		var name = data[i].name;
